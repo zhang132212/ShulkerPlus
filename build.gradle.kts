0591262b0c692c9f8e71d5d39595224e58dcf187
zhang132212 <3318143775@qq.com>
2026-07-02 13:32:11 +0800

    Add paperweight-userdev, CI build+release workflow, fix bundle mode for containers

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,20 @@
 plugins {
     `java-library`
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
 }
 
 group = "me.zhang132212"
 version = "3.0.0"
 
-repositories {
-    mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
-}
-
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.11-R0.1-SNAPSHOT")
 }
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
+
+paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
 
 tasks.processResources {
     filesMatching("plugin.yml") {
